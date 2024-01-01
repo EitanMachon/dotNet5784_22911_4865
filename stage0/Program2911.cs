d825549f4221c7a0bd6ad2c4c5f3716a4224cd6a
EitanMachon <eitanlattes@gmail.com>
2024-01-01 15:03:07 +0200

    intial code + welcome function

---
+++ stage0/Program2911.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome2911();
+        Welcome4865();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome4865();
+   
+    private static void Welcome2911()
+    {
+        Console.WriteLine("Enter your name: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first cnsole application", username);
+    }
+}
