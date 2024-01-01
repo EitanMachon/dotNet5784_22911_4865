6641be145139578cd6ed69f3457c2eda77ed9817
Yishay <ishayshlomotiram@gmail.com>
2024-01-01 15:20:47 +0200

    new class

---
+++ stage0/Program4865.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program
+    {
+        static partial void Welcome4865 ()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
