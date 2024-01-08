a3dee9be9b6a1aac09491c2c0ede81912bc86232
Yishay Tiram <ytiram@g.jct.ac.il>
2024-01-08 21:36:25 +0200

    end of stage 4

---
+++ DalFacade/DO/Depandency.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DO;
+
+public record Depandency
+(
+     int id,
+     int DependentTask,
+     int Depends
+)
+{
+    public Depandency() : this(0, 0, 0) { }
+}
---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,23 @@
+﻿namespace DO;
+
+public record Task
+(
+   int Id,
+   string Alias,
+   string Description,
+   DateTime CreatedAtDate,
+   TimeSpan RequiredEffort,
+   bool IsMilestone,
+   EngineerExperience Copmlexity,
+   DateTime StartDate,
+   DateTime ScheduledTime,
+   DateTime DeadLinetime,
+   DateTime ComplateTime,
+   string Dekiverables,
+   string Remarks,
+   int EngineerId,
+   EngineerExperience Difficulty
+)
+{ 
+    public Task():this(0, "", "", DateTime.Now, TimeSpan.Zero, false, EngineerExperience.Beginner, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now, "", "", 0, EngineerExperience.Beginner) { }
+}
