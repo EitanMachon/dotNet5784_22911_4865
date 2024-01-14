a70cd039f47c24a2e33955b31981de3b651a83ad
Yishay Tiram <ytiram@g.jct.ac.il>
2024-01-14 12:09:21 +0200

    3+4

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DalApi;
+using DO;
+//namespace DO;
+//namespace DalApi;
+//namespace Dal;
+
+public interface IDal
+{
+    IDependency idependancy { get; } // property to access the dependency DAL object
+    IEngineer iengineer { get; } // property to access the engineer DAL object
+    ITask task { get; } // property to access the task DAL object
+}
---
+++ DalList/DalList.cs
@@ -0,0 +1,11 @@
+﻿namespace Dal;
+using DalApi;
+sealed public class DalList : IDal
+{
+    public IDependency idependancy => new DependencyImplementation();
+
+    public IEngineer iengineer => new EngineerImplementation();
+
+    public ITask task => new TaskImplementaion();
+}
+
