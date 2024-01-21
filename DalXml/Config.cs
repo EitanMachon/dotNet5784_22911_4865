8616181714b380b51a93cfac753b4e413fbf528d
EitanMachon <eitanlattes@gmail.com>
2024-01-21 14:01:47 +0200

    start of part triee

---
+++ DalXml/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal class Config
+    {
+        static string s_data_config_xml = "data-config";
+        internal static int NextId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextId"); }
+        internal static int NextId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextId"); }
+    }
+}
