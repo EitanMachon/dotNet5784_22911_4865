65fc6b7a160958a3ea60f347409ed21e19cf179b
Yishay Tiram <ytiram@g.jct.ac.il>
2024-01-31 00:00:48 +0200

    end of stage 6

--- BL/BO/Task.cs
+++ BL/BO/Task.cs
@@ -8,22 +8,22 @@ namespace BO;
 
 public class Task
 {
-    public int Id { get; init; }
-    public string Alias { get; set; }
-    public string Description { get; set; } 
-    public DateTime CreatedAtDate { get; set; }
+    public int Id { get; init; } // this is the id of the task
+    public string Alias { get; init; } // this is the alias of the task
+    public string Description { get; set; } // this is the description of the task
+    public DateTime CreatedAtDate { get; set; } // this is the date the task was created
 
-    public TimeSpan RequiredEffort { get; set; }
-    public bool IsMilestone { get; set; }
+    public TimeSpan RequiredEffort { get;  } // this is the required effort of the task
+    public bool IsMilestone { get; set; } // this is the milestone of the task
 
-    public EngineerExperience { get; set; }
-public DateTime? StartDate { get; set; }
-public DateTime? ScheduledTime { get; set; }
-public DateTime? DeadLinetime { get; set; }
-public DateTime? ComplateTime { get; set; }
-public string Dekiverables { get; set; }
-public string Remarks { get; set; }
-public int EngineerId { get; set; }
-public EngineerExperience Difficulty { get; set; }
+ public EngineerExperience Copmlexity { get; set; } // this is the complexity of the task   
+public DateTime? StartDate { get; }  // this is the start date of the task
+public DateTime? ScheduledTime { get; set; } // this is the scheduled time of the task
+public DateTime? DeadLinetime { get; set; } // this is the deadline time of the task
+public DateTime? ComplateTime { get; } // this is the complete time of the task
+public string Dekiverables { get; set; } // this is the deliverables of the task
+public string Remarks { get; set; } // this is the remarks of the task
+public int EngineerId { get; set; } // this is the id of the engineer
+public EngineerExperience Difficulty { get; set; } // this is the difficulty of the task
 
 }
---
+++ BL/BlApi/ITak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+/// <summary>
+///  this is the interface of the BL layer that is used to implement the BL layer  functions of the Task
+/// </summary>
+public interface ITak
+{
+    public int createTask(BO.Task task); // this function get a task and create it in the database
+
+    public BO.Task? read(int id); // this function get an id and return the task that match the id
+
+    public BO.Task? read(Func<BO.Task, bool> filter); // this func get a filter and return the first item that match the filter
+
+    public IEnumerable<BO.Task?> readAll(Func<BO.Task, bool>? filter = null); // this function return all the tasks in the database
+
+    public void update(BO.Task task); // this function get a task and update it in the database
+
+    public void delete(BO.Task task); // this function get a task and delete it from the database 
+}
