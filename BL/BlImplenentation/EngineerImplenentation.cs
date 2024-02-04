5de2b852b38e3a9b5fefffb29000dcc9b04871fe
Yishay Tiram <ytiram@g.jct.ac.il>
2024-02-04 20:51:07 +0200

    פרםערקד ןמ דאשעק 8

--- BL/BlImplenentation/EngineerImplenentation.cs
+++ BL/BlImplenentation/EngineerImplenentation.cs
@@ -3,36 +3,82 @@ using DalApi;
 using DO;
 internal class EngineerImplenentation : IEngineer
 {
-    private IDal dal = new DalApi.IDal();
+    private IDal dal = Factory.Get; // create a new instance of the DAL layer to use its functions to implement the BL layer functions like Create, Delete, Read, ReadAll, and Update
     //DalApi.IDal dal = new DalApi.DalApi();
     public int Create(Engineer item)
     {
-        throw new NotImplementedException();
+        try
+        {
+            int idEngineer = Create(item); // create a new Engineer
+            return idEngineer; // return the new ID of the Engineer
+        }
+        catch (DO.DalAlreadyExistsException e) // if the Engineer already exists, throw an exception
+        {
+            throw new Exception("Engineer with the same ID already exists", e); // throw an exception
+        }
+       
 
     }
 
-    public void Delete(int id)
+    public void Delete(int id) // delete an Engineer by his ID
     {
-        throw new NotImplementedException();
+        try // try to delete the Engineer
+        {
+            Delete(id); // delete the Engineer with the given ID in the DAL layer
+        }
+        catch (DO.DalDeletionImpossible e) // if the Engineer cannot be deleted, throw an exception
+        {
+            throw new Exception("Engineer with the given ID does not exist", e); // throw an exception
+        }
+        
     }
 
-    public Engineer? Read(int id)
+    public Engineer? Read(int id) // read an Engineer by his ID
     {
-        throw new NotImplementedException();
+        if (id == 0) // if the ID is 0, throw an exception because it is not possible to read an Engineer with ID 0
+        {
+            throw new DO.NotFoundId("Engineer with ID 0 does not exist"); // throw an exception
+        }
+        
+        return new Engineer(); // this is a temporary return value to avoid compilation errors
     }
 
     public Engineer? Read(Func<Engineer, bool> filter)
     {
-        throw new NotImplementedException();
+        if (filter == null) // if the filter is null, throw an exception because it is not possible to filter the list
+        {
+            throw new Exception("Filter is null");
+        }
+        return new Engineer(); // this is a temporary return value to avoid compilation errors
     }
 
     public IEnumerable<Engineer?> ReadAll(Func<Engineer, bool>? filter = null)
     {
-        throw new NotImplementedException();
+       if (filter != null) // if the filter is not null, return a list of Engineers that match the filter
+        {
+            return ReadAll(filter); // return a list of Engineers that match the filter
+        }
+        return ReadAll(); // return a list of all Engineers 
+        
     }
 
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        if (item == null) // if the Engineer is null, throw an exception because it is not possible to update a null Engineer
+        {
+            throw new Exception("Engineer is null");
+        }
+        if (item.Id == 0) // if the Engineer has no ID, throw an exception
+        {
+            throw new DO.NotFoundId("Engineer with ID 0 does not exist"); // throw an exception
+        }
+        try // try to update the Engineer
+        {
+            Update(item); // update the Engineer
+        }
+        catch (DO.DalDeletionImpossible e) // if the Engineer cannot be updated, throw an exception
+        {
+            throw new Exception("Engineer with the given ID does not exist", e); // throw an exception
+        }
     }
 }
--- BL/BlImplenentation/TaskImplementation.cs
+++ BL/BlImplenentation/TaskImplementation.cs
@@ -1,41 +1,83 @@
 ﻿namespace BlImplenentation;
-using BlApi;
+
 using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 internal class TaskImplementation : ITask
 {
-    public int Create(Task item)
-    { 
-        throw new NotImplementedException();
-    }    
-
-
-    public void Delete(int id)
+    private IDal dal = Factory.Get; // create a new instance of the DAL layer to use its functions to implement the BL layer functions like Create, Delete, Read, ReadAll, and Update
+    public int Create(Task item) // create a new Task
     {
-        throw new NotImplementedException();
+        try
+        {
+            int idTask = Create(item); // create a new Task
+            return idTask; // return the new ID of the Task
+        }
+        catch (DO.DalAlreadyExistsException e) // if the Task already exists, throw an exception
+        {
+            throw new Exception("Task with the same ID already exists", e); // throw an exception
+        }
     }
 
-    public Task? Read(int id)
+
+    public void Delete(int id) // delete a Task by his ID
     {
-        throw new NotImplementedException();
+        try // try to delete the Task
+        {
+            Delete(id); // delete the Task with the given ID in the DAL layer
+        }
+        catch (DO.DalDeletionImpossible e) // if the Task cannot be deleted, throw an exception
+        {
+            throw new Exception("Task with the given ID does not exist", e); // throw an exception
+        }
     }
 
-    public Task? Read(Func<Task, bool> filter)
+    public Task? Read(int id) // read a Task by his ID
     {
-        throw new NotImplementedException();
+        if (id == 0) // if the ID is 0, throw an exception because it is not possible to read a Task with ID 0
+        {
+            throw new DO.NotFoundId("Task with ID 0 does not exist"); // throw an exception
+        }
+        return new Task(); // this is a temporary return value to avoid compilation errors
     }
 
-    public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
+    public Task? Read(Func<Task, bool> filter) // read a Task by a filter
     {
-        throw new NotImplementedException();
+        if (filter == null) // if the filter is null, throw an exception because it is not possible to filter the list
+        {
+            throw new Exception("Filter is null");
+        }
+        return new Task(); // this is a temporary return value to avoid compilation errors
     }
 
-    public void Update(Task item)
+    public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null) // read all Tasks
     {
-        throw new NotImplementedException();
+        if (filter != null) // if the filter is not null, return a list of Tasks that match the filter
+        {
+            return ReadAll(filter); // return a list of Tasks that match the filter
+        }
+        return ReadAll(); // return a list of all Tasks 
+    }
+
+    public void Update(Task item)  // update a Task
+    {
+        if (item == null) // if the Task is null, throw an exception because it is not possible to update a null Task
+        {
+            throw new Exception("Task is null");
+        }
+        if (item.Id == 0) // if the Task has no ID, throw an exception
+        {
+            throw new DO.NotFoundId("Task with ID 0 does not exist"); // throw an exception
+        }
+        try // try to update the Task
+        {
+            Update(item); // update the Task in the DAL layer
+        }
+        catch (DO.DalUpdateImpossible e) // if the Task cannot be updated, throw an exception
+        {
+            throw new Exception("Task with the given ID does not exist", e); // throw an exception
+        }
     }
 }
