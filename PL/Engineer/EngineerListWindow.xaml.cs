960b630407efd7931ee8e3afc0e317a9d5b1817c
Yishay Tiram <ytiram@g.jct.ac.il>
2024-02-13 02:07:09 +0200

    part 7 finished

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -20,24 +20,34 @@ namespace PL.Engineer;
 public partial class EngineerListWindow : Window
 {
     static readonly BlApi.IBl s_bl = BlApi.Factory.Get(); // Create a new instance of the BlApi class and store it in a static readonly variable 
-
+    /// <summary>
+    ///  the function that gonna show the EngineerListWindow and the list of the engineers by using the DependencyProperty that is a static member of the EngineerListWindow class
+    /// </summary>
     public IEnumerable<BO.Engineer> EngineerList
     {
-        get { return (IEnumerable<BO.>)GetValue(CourseListProperty); }
-        set { SetValue(CourseListProperty, value); }
+        get { return (IEnumerable<BO.Engineer>)GetValue(EngineerListProperty); } // Using GetValue and SetValue to get and set the value of the EngineerList property
+        set { SetValue(EngineerListProperty, value); } // Using GetValue and SetValue to get and set the value of the EngineerList property
     }
-
-    public static readonly DependencyProperty CourseListProperty =
-        DependencyProperty.Register("CourseList", typeof(IEnumerable<BO.CourseInList>), typeof(CourseListWindow), new PropertyMetadata(null));
+    /// <summary>
+    ///  the list of the engineers that we gonna show in the window by using the DependencyProperty that is a static member of the EngineerListWindow class
+    /// </summary>
+    public static readonly DependencyProperty EngineerListProperty =
+        DependencyProperty.Register("CourseList", typeof(IEnumerable<BO.Engineer>), typeof(EngineerWindow), new PropertyMetadata(null)); // Using DependencyProperty as the backing store for EngineerList.  This enables animation, styling, binding, etc...
 
 
     /// <summary>
     /// this func gonna show the EngineerListWindow
     /// </summary>
-    public EngineerListWindow()
+    public EngineerListWindow() // the constructor of the EngineerListWindow class
     {
-        InitializeComponent();
+        InitializeComponent(); // Initialize the EngineerListWindow
+        EngineerList = s_bl?.Engineer.ReadAll()!; // Using the BlApi to get all the engineers and store them in the EngineerList
+    }
+    public BO.EngineerExperience Level { get; set; } = BO.EngineerExperience.Beginner; // Create a new instance of the BO.EngineerExperience class and store it in a property and give it a diffult value of BO.EngineerExperience.Beginner
+
+    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        EngineerList = s_bl?.Engineer.ReadAll()!.Where(x => x.Level == Level); // Using the BlApi to get all the engineers and store them in the EngineerList and filter them by the Level
     }
     
-  
 }
---
+++ PL/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PL;
+/// <summary>
+/// this class conect the PL and the Enums of the BL by using the IEnumerable by using the GetEnumerator function
+/// </summary>
+internal class LevelCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.EngineerExperience> s_enums = (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!; // Create a new instance of the IEnumerable<BO.EngineerExperience> class and store it in a static readonly variable
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator(); // the function that gonna return the IEnumerator of the s_enums
+}
