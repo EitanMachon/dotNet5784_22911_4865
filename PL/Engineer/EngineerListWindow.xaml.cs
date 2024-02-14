33f283fba5de3299093149ffcec0429dfa2e45ba
Yishay Tiram <ytiram@g.jct.ac.il>
2024-02-15 00:18:46 +0200

    after i added doc

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -21,7 +21,9 @@ namespace PL.Engineer
     public partial class EngineerListWindow : Window
     {
         static readonly IBl s_bl = Factory.Get(); // Use IBl interface instead of BlApi class
-
+        /// <summary>
+        /// this function is used to get the list of engineers
+        /// </summary>
         public IEnumerable<BO.Engineer> EngineerList
         {
             get { return (IEnumerable<BO.Engineer>)GetValue(EngineerListProperty); }
@@ -30,42 +32,53 @@ namespace PL.Engineer
 
         public static readonly DependencyProperty EngineerListProperty =
             DependencyProperty.Register("EngineerList", typeof(IEnumerable<BO.Engineer>), typeof(EngineerListWindow), new PropertyMetadata(null));
-
+        /// <summary>
+        /// this function is used to initialize the engineer list window
+        /// </summary>
         public EngineerListWindow()
         {
             InitializeComponent();
             EngineerList = s_bl?.Engineer.ReadAll()!;
         }
-
+        
         public BO.EngineerExperience Level { get; set; } = BO.EngineerExperience.All;
-
+        /// <summary>
+        /// this function is used to update the list of engineers in the window
+        /// </summary>
+        
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Update();
+            Update(); // Call the Update function to update the list of engineers
         }
-
+        /// <summary>
+        /// this function is used to add a new engineer to the list when the add button is clicked
+        /// </summary>
         private void Button_Add_click(object sender, RoutedEventArgs e)
         {
             EngineerWindow engineerWindow = new EngineerWindow();
             engineerWindow.ShowDialog();
             Update();
         }
-
+        /// <summary>
+        /// when we double click on an engineer in the list, we open the engineer window to edit the engineer details
+        /// </summary>
         private void twoclicksbuttom(object sender, MouseButtonEventArgs e)
         {
-            BO.Engineer? engineerFromList = (sender as ListView)?.SelectedItem as BO.Engineer;
+            BO.Engineer? engineerFromList = (sender as ListView)?.SelectedItem as BO.Engineer; 
             if (engineerFromList != null)
             {
                 new EngineerWindow(engineerFromList!.Id).ShowDialog();
                 Update();
             }
         }
-
+        /// <summary>
+        /// this function is used to update the list of engineers in the window
+        /// </summary>
         void Update()
         {
-            if (Level == BO.EngineerExperience.All)
+            if (Level == BO.EngineerExperience.All) // If the level is all, we get all the engineers
                 EngineerList = s_bl?.Engineer.ReadAll()?.OrderBy(item => item?.Name); // Corrected orderBy usage
-            else
+            else // If the level is not all, we get the engineers with the specified level
                 EngineerList = s_bl?.Engineer.ReadAll(x => x != null && x.Level == Level)?.OrderBy(item => item?.Name);
         }
     }
