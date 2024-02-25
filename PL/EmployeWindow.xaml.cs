b1ecd5d6aaad646ae03f6be8b510414eb892e483
Yishay Tiram <ytiram@g.jct.ac.il>
2024-02-25 22:27:56 +0200

    i start to work on employee task page

--- PL/EmployeWindow.xaml.cs
+++ PL/EmployeWindow.xaml.cs
@@ -23,25 +23,26 @@ namespace PL
         /// <summary>
         /// this function is used to get the list of engineers
         /// </summary>
-        public IEnumerable<BO.Engineer> EngineerList
+        public BO.Task employeTask // Create a new instance of the BO.Task class and store it in a property
         {
-            get { return (IEnumerable<BO.Engineer>)GetValue(EngineerListProperty); }
-            set { SetValue(EngineerListProperty, value); }
+            get { return (BO.Task)GetValue(EmployeProperty); }
+            set { SetValue(EmployeProperty, value); }
+        }
+        public static readonly DependencyProperty EmployeProperty = DependencyProperty.Register("Employe", typeof(BO.Task), typeof(EmployeWindow), new PropertyMetadata(null)); // Using DependencyProperty as the backing store for Employe.  This enables animation, styling, binding, etc...
+        
+        public EmployeWindow(int i = 0) // the constructor of the EmployeWindow class that get a parameter with a default value of 0
+        {
+            InitializeComponent(); // Initialize the EmployeWindow
+            if (i == 0) // if the id of the task is equal to 0
+            {
+                employeTask = new BO.Task(); // Create a new instance of the BO.Task class and store it in a property and give it a diffult value of 0
+            }
+            else // if the id of the task is not equal to 0
+            {
+                employeTask = s_bl?.Task.Read(i)!; // Using the BlApi to get the task by the id and store it in the employeTask
+            }
         }
 
-        private void SetValue(object engineerListProperty, IEnumerable<BO.Engineer> value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public EmployeWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
+        
     }
 }
